10b2859b08d195a7fe89953f9251f60eae73d286
xuanhoang2702 <doxuanhoang802@gmail.com>
2022-08-19 11:11:13 +0700

    css_admin&client

--- resources/views/admin/doctor.blade.php
+++ resources/views/admin/doctor.blade.php
@@ -1,46 +1,46 @@
 @extends('layouts.layout_admin_master')
 
 @section('content')
-    @if (session('msg'))
-        <div class="alert alert-success mb-1 mt-1">
-            {{ session('msg') }}
-        </div>
-    @endif
-    <h1>Quản lý bác sĩ</h1>
-    <table class="table">
-        <thead>
-            <tr>
-                <th>#</th>
-                <th width='20%'>Image</th>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Level</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @php($stt = 1)
-            @foreach ($listDoctor as $doctor)
-                <tr>
-                    <th>{{ $stt++ }}</th>
-                    <td><img width="100%" src="{{ asset('img/ducson.jpg') }}" alt=""></td>
-                    <td>{{ $doctor->name }}</td>
-                    <td>{{ $doctor->email }}</td>
-                    <td>{{ $doctor->level }}</td>
-                    <td>
-                        <form action="{{ route('admin.doctor.edit', $doctor->id) }}" method="get">
-                            @csrf
-                            <button type="submit" class="btn btn-primary">Edit</button>
-                        </form>
-                        {{-- <form action="{{ route('admin.doctor.destroy', $doctor->id) }}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Delete</button>
-                        </form> --}}
-                        @include('includes.delete', ['route'=> 'admin.doctor.destroy','id'=> $doctor->id]);
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+@if (session('msg'))
+<div class="alert alert-success mb-1 mt-1">
+    {{ session('msg') }}
+</div>
+@endif
+<h1>Quản lý bác sĩ</h1>
+<table class="table">
+    <thead>
+        <tr>
+            <th>#</th>
+            <th width='20%'>Image</th>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Level</th>
+            <th>Action</th>
+        </tr>
+    </thead>
+    <tbody>
+        @php($stt = 1)
+        @foreach ($listDoctor as $doctor)
+        <tr>
+            <th>{{ $stt++ }}</th>
+            <td><img width="100px" src="{{ asset('img/ducson.jpg') }}" alt=""></td>
+            <td>{{ $doctor->name }}</td>
+            <td>{{ $doctor->email }}</td>
+            <td>{{ $doctor->level }}</td>
+            <td>
+                <form action="{{ route('admin.doctor.edit', $doctor->id) }}" method="get">
+                    @csrf
+                    <button type="submit" style="width:70px ;" class="btn btn-primary">Edit</button>
+                </form>
+                {{-- <form action="{{ route('admin.doctor.destroy', $doctor->id) }}" method="post">
+                @csrf
+                @method('DELETE')
+                <button type="submit" style="width:70px ;" class="btn btn-danger">Delete</button>
+                </form> --}}
+                @include('includes.delete', ['route'=> 'admin.doctor.destroy','id'=> $doctor->id]);
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+</table>
 @endsection
--- resources/views/clients/ajaxSearch.blade.php
+++ resources/views/clients/ajaxSearch.blade.php
@@ -1,19 +1,21 @@
 @if ($allDoctorSearch->count() > 0)
-    <div class="container">
-        <div class="row d-flex">
-            @foreach ($allDoctorSearch as $key => $value)
-                <div class="itemDoctor d-flex">
-                <img src="{{ asset('img/ducson.jpg') }}" alt="abc">
-                    <div>
-                        <p> {{ $value->name }} </p>
-                        <p> {{ $value->email }}</p>
-                    </div>
-                </div>
-            @endforeach
+<div class="container">
+    <h3 style="font-size:26px ;margin-top:30px ; margin-bottom:40px ;font-weight: bold;">Kết quả tìm kiếm</h3>
+    <div class="row d-flex" id="search_doctor">
+        @foreach ($allDoctorSearch as $key => $value)
+        <div class="itemDoctor d-flex">
+            <img src="{{ asset('img/ducson.jpg') }}" alt="abc">
+            <div>
+                <h2 style="font-size:24px ;padding-left:190px ;font-weight: bold;padding-top: 20px;">Thông tin bác sĩ</h2>
+                <p class="info-doctor"> {{ $value->name }} </p>
+                <p class="info-doctor"> {{ $value->email }}</p>
+            </div>
         </div>
+        @endforeach
     </div>
+</div>
 @else
-    <div class="container text-center">
-       <h1> No Data Found </h1>
-    </div>
+<div class="container text-center">
+    <h1> No Data Found </h1>
+</div>
 @endif
